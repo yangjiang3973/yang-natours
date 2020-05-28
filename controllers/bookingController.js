48ce7d0e2124a8fae808bba73b50298492b94246
IanJiang0817 <yang3973@gmail.com>
2020-05-28 17:28:18 +0800

    finished this project!!

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -24,7 +24,7 @@ module.exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        // images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        // images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
         amount: tour.price * 100,
         currency: "usd",
         quantity: 1
